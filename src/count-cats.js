185c3e8fb3d7cc1b6b0b5cc74d10b1e706644f67
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-03-06 21:03:14 +0300

    feat: finish countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  //let a="^^";
+  
+const arr = matrix.reduce((prev, item) => {
+return prev.concat(item);
+}, []);
+//return flat;
+let cats=0;
+for(i=0;i<arr.length;i++){
+  if(arr[i]=="^^"){
+    cats=cats+1;
+  }
+}
+return cats;
 };
