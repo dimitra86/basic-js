442909c270be7def8ebaf96d9a2a6b2b47d05583
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-03-08 21:20:42 +0300

    feat: dream team finish

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+ if (Array.isArray(members)) {
+   let name=[];
+   for(let item of members){
+     if (typeof item==="string"){
+       const arrItem=item.trim().split('');
+       name.push(arrItem[0].toUpperCase())
+     }
+   }
+   return name.sort().join('')
+ } else {
+   return false
+ }
+}
