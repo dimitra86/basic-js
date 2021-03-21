1ad7e7620493de2ead2b63666061acc340f2d707
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-03-21 15:15:04 +0300

    feat: finish extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    //throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+
+
+    let result = String(str);
+    let str2 = '';
+    if (options.addition !== undefined) {
+        result += options.addition;
+    }
+    if (options.additionRepeatTimes > 1) {
+        for (let i = 1; i < options.additionRepeatTimes; i++) {
+            result += (options.additionSeparator || "|") + options.addition;
+        }
+    }
+    if (options.repeatTimes > 1) {
+        for (let i = 1; i < options.repeatTimes; i++) {
+            str2 += result + (options.separator || "+");
+        }
+        str2 += result;
+    } else {
+        str2 = result;
+    }
+    return str2;
+
+
+}
