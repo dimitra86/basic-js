e09b05757555b8b8a7ebfbb6ad31fb7d193df98d
Dzmitry Nikalayeu <dimitra86@list.ru>
2021-03-27 10:39:38 +0300

    feat: hanoi finish two

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,13 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  let numTransfers = 2**disksNumber - 1; 
-  let timeTransfers=Math.floor(numTransfers * 3600 / turnsSpeed);
-  let answer = {     // объект
-    turns: numTransfers,  
-    seconds: timeTransfers        
-  }
-  return answer;
+    //throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    //  let numTransfers = 2**disksNumber - 1; 
+    //  let timeTransfers=Math.floor(numTransfers * 3600 / turnsSpeed);
+    //  let answer = {     // объект
+    //    turns: numTransfers,  
+    //    seconds: timeTransfers        
+    //  }
+    //  return answer;
+
+    //variant two
+    const turnsPerSecond = turnsSpeed / 3600;
+    const turns = (2 ** disksNumber) - 1;
+    const seconds = Math.floor(turns / turnsPerSecond);
+
+    return {
+        turns,
+        seconds
+    };
 };
