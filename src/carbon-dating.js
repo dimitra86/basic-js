bbc0ad73a73e92e0322ae2563dafdcf126081d08
Dzmitry Nikalayeu <dimitra86@list.ru>
2023-12-19 22:52:34 +0300

    feat: add one task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let num=Number(sampleActivity);
+  
+  if((typeof(sampleActivity)!=="string")|(num<=0)|(num>=15)|(Boolean(num)==false)){
+    return false;
+  }
+
+else  return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/(0.693/HALF_LIFE_PERIOD));
 }
 
 module.exports = {
