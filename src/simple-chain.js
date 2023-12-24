8e799ac39c015a1c6d3d4dbb5737ff4ecde6e819
Dzmitry Nikalayeu <dimitra86@list.ru>
2023-12-24 17:04:35 +0300

    feat: add 2 task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  currChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.currChain.push(`( ${value} )`);
+    return this;
   },
   addLink(/* value */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this._checkIntegerNumber(position)) {
+      if (position < 1 || position > this.getLength()) {
+          this._deleteChain();
+          throw new Error('Removing link position is out of range!');
+      }
+      const index = position - 1;
+      this.currChain.splice(index, 1);
+      return this;
+  }
+  this._deleteChain();
+  throw new Error('Position must be integer number!');
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.currChain.reverse();
+        return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const chain = this._getChain();
+    this._deleteChain();
+    return chain;
+},
+_deleteChain() {
+    this.currChain.length = 0;
+},
+_checkIntegerNumber(num) {
+    return typeof num === 'number' && (num ^ 0) === num;
+},
+_getChain() {
+    return this.currChain.join('~~');
   }
 };
 
