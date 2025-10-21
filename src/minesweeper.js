bb6667f21301e2ecd7ab55b11cbc355c32dc242b
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-21 20:24:21 +0300

    feat: add minesweeper

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -23,9 +23,38 @@ const { NotImplementedError } = require('../lib');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const result = Array(rows).fill(0).map(() => Array(cols).fill(0));
+
+  const directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1],           [0, 1],
+    [1, -1],  [1, 0],  [1, 1]
+  ];
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j]) {
+        continue;
+      }
+
+      let count = 0;
+      for (const [dx, dy] of directions) {
+        const x = i + dx;
+        const y = j + dy;
+
+        if (x >= 0 && x < rows && y >= 0 && y < cols && matrix[x][y]) {
+          count++;
+        }
+      }
+
+      result[i][j] = count;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
