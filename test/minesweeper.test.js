51b92708091733539e4c0cf213ac7bd95c5680f3
Dzmitry Nikalayeu <dimitra86@list.ru>
2025-10-21 20:30:15 +0300

    feat: add minesweeper

--- test/minesweeper.test.js
+++ test/minesweeper.test.js
@@ -1,38 +1,60 @@
-const { describe } = require('node:test');
-const assert = require('node:assert');
-const { test } = require('../lib');
-const { minesweeper } = require('../src/minesweeper.js');
+const { NotImplementedError } = require('../lib');
 
-describe('Minesweeper', () => {
-  // Presence requirement
-  test('function minesweeper exists', () => {
-    assert.strictEqual(typeof minesweeper, 'function');
-  });
+/**
+ * In the popular Minesweeper game you have a board with some mines and those cells
+ * that don't contain a mine have a number in it that indicates the total number of mines
+ * in the neighboring cells. Starting off with some arrangement of mines
+ * we want to create a Minesweeper game setup.
+ *
+ * @param {Array<Array>} matrix
+ * @return {Array<Array>}
+ *
+ * @example
+ * matrix = [
+ *  [true, false, false],
+ *  [false, true, false],
+ *  [false, false, false]
+ * ]
+ *
+ * The result should be following:
+ * [
+ *  [1, 2, 1],
+ *  [2, 1, 1],
+ *  [1, 1, 1]
+ * ]
+ */
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const result = Array(rows).fill(0).map(() => Array(cols).fill(0));
 
-  // Functional requirements
-  test('should return minesweeper game setup', () => {
-    assert.deepStrictEqual(
-      minesweeper([
-        [true, false, false],
-        [false, true, false],
-        [false, false, false],
-      ]),
-      [
-        [1, 2, 1],
-        [2, 1, 1],
-        [1, 1, 1],
-      ],
-    );
+  const directions = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0, -1],           [0, 1],
+    [1, -1],  [1, 0],  [1, 1]
+  ];
 
-    assert.deepStrictEqual(
-      minesweeper([
-        [false, false, false],
-        [false, false, false],
-      ]),
-      [
-        [0, 0, 0],
-        [0, 0, 0],
-      ],
-    );
-  });
-});
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (!matrix[i][j]) {
+        let count = 0;
+        for (const [dx, dy] of directions) {
+          const x = i + dx;
+          const y = j + dy;
+
+          if (x >= 0 && x < rows && y >= 0 && y < cols && matrix[x][y]) {
+            count++;
+          }
+        }
+
+        result[i][j] = count;
+      }
+    }
+  }
+
+  return result;
+}
+
+module.exports = {
+  minesweeper
+};
